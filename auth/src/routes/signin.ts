875da1a049a504ad3ba3be6c3e8e094f167d01da
simon garber <simon.garber@gmail.com>
2020-06-23 16:48:45 -0400

    abstracted request validation error logic into middleware for request validation, applied it to signup and signin routes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+
 const router = express.Router();
 
 router.post(
@@ -9,14 +10,9 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password").trim().notEmpty().withMessage("Must supply a password"),
   ],
+  validateRequest,
 
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-  }
+  (req: Request, res: Response) => {}
 );
 
 export { router as signinRouter };
