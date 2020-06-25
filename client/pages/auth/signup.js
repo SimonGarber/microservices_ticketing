d28604dbed178ddb4a37dfb00a3af118d1ccb8a3
simon garber <simon.garber@gmail.com>
2020-06-25 15:45:09 -0400

    modified useRequest hook to handle success cases that trigger redirect on a successful response for signing in a user

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,24 +1,24 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
+import useRequest from "../../hooks/useRequest";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    setErrors([]);
-    try {
-      const response = await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
 
-      console.log(response.data);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    await doRequest();
   };
   return (
     <form onSubmit={onSubmit}>
@@ -28,6 +28,7 @@ export default () => {
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          type="email"
           className="form-control"
         />
       </div>
@@ -36,19 +37,11 @@ export default () => {
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          type="password"
           className="form-control"
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops....</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
