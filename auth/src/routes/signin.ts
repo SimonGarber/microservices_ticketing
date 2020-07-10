e04dd69c8bea1bc8f329147d2c84b1fffa8f5178
simon garber <simon.garber@gmail.com>
2020-07-09 22:17:17 -0400

    created scripts for updating the common module, publishing to NPM and handling git commits and pushes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,8 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { User } from "../models/user";
 import jwt from "jsonwebtoken";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@ticketr/common";
 import { Password } from "../services/password";
 
 const router = express.Router();
